b46a06bd1067f2db5bc3e61a48a3bdeb92e599d8
Thomas Lin <t.lin@mail.utoronto.ca>
2014-06-06 05:00:36 -0400

    Fix bug with Enqueue when FlowSpace entry matches contain a mix of input ports and src/dst fields

--- src/org/flowvisor/message/actions/FVActionEnqueue.java
+++ src/org/flowvisor/message/actions/FVActionEnqueue.java
@@ -6,6 +6,8 @@ import org.flowvisor.classifier.FVClassifier;
 import org.flowvisor.exceptions.ActionDisallowedException;
 import org.flowvisor.flows.FlowEntry;
 import org.flowvisor.flows.SliceAction;
+import org.flowvisor.log.FVLog;
+import org.flowvisor.log.LogLevel;
 import org.flowvisor.openflow.protocol.FVMatch;
 import org.flowvisor.slicer.FVSlicer;
 import org.openflow.protocol.OFMatch;
@@ -23,7 +25,18 @@ public class FVActionEnqueue extends OFActionEnqueue implements SlicableAction {
 		/*
 		 * Match OFMatch, if flowentry has queue id then OK. 
 		 */
+		/*
 		FVMatch neoMatch = new FVMatch(match);
+		neoMatch.setInputPort(this.port); // This causes a bug if flowspaces specify in_port. Enqueue action targets output ports.
+                                            // Filtering flowspaces by the match /w in_port replaced by target output port doesn't make sense... it switches up src/dst
+                                            // Fix isn't trivial; Cannot simply swap src/dst because FV we don't know what fields are specified...
+                                            // Fix: Simply filter flowspace entries by output port; Less efficient though...
+		FVLog.log(LogLevel.WARN, fvSlicer, "    IN ENQUEUE, MATCH IS: " + match + " and wildcards are: " + match.getWildcards());
+		FVLog.log(LogLevel.WARN, fvSlicer, "    IN ENQUEUE, NEOMATCH IS: " + neoMatch + " and wildcards are: " + neoMatch.getWildcards());
+		List<FlowEntry> entries = fvSlicer.getFlowSpace().matches(fvClassifier.getDPID(), neoMatch);
+		*/
+		FVMatch neoMatch = new FVMatch();
+		neoMatch.setWildcards(neoMatch.getWildcards() & ~FVMatch.OFPFW_IN_PORT); // Only match is input port
 		neoMatch.setInputPort(this.port);
 		List<FlowEntry> entries = fvSlicer.getFlowSpace().matches(fvClassifier.getDPID(), neoMatch);
 		for (FlowEntry fe : entries) {
@@ -36,6 +49,7 @@ public class FVActionEnqueue extends OFActionEnqueue implements SlicableAction {
 				}
 			}
 		}
+		//FVLog.log(LogLevel.WARN, fvSlicer, ".......... IN ENQUEUE... uh oh!... ");
 		throw new ActionDisallowedException("Slice " + 
 				fvSlicer.getSliceName() + " may not enqueue to queue " + this.queueId
 				+ " for port " + this.port,
